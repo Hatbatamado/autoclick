d2210aff0aa35fa40841d37cdae19d2218aa68b6
Nakia <nakiaaresz@gmail.com>
2014-07-07 21:25:29 +0200

    delete lane done & some idiot protection

--- Src/Form1.cs
+++ Src/Form1.cs
@@ -291,17 +291,40 @@ namespace click
         int elements = 0;
         private void Add()
         {
-            uint X = Convert.ToUInt32(x.Text);
-            uint Y = Convert.ToUInt32(y.Text);
-            int d = Convert.ToInt32(delay.Text);
-            int r = Convert.ToInt32(repeat.Text);
-            //--------------------------------------------------- 
-            click.Add(new Click(elements, X, Y, d, r));
-            elements++;
-            rt.Text = rt.Text + click[click.Count - 1].Click_Out() + '\n';
-            //--------------------------------------------------- 
-            rt.SelectionStart = rt.Text.Length;
-            rt.ScrollToCaret();
+            int X = -1;
+            int Y = -1;
+            int d = -1;
+            int r = -1;
+            try
+            {
+                X = Convert.ToInt32(x.Text);
+            }
+            catch (FormatException) { }
+            try
+            {
+                Y = Convert.ToInt32(y.Text);
+            }
+            catch (FormatException) { }
+            try
+            {
+                d = Convert.ToInt32(delay.Text);
+            }
+            catch (FormatException) { }
+            try
+            {
+                r = Convert.ToInt32(repeat.Text);
+            }
+            catch (FormatException) { }
+            if (X != -1 && Y != -1 && d != -1 && r != -1)
+            {
+                //--------------------------------------------------- 
+                click.Add(new Click((uint)X, (uint)Y, d, r));
+                rt.Text = rt.Text + click[click.Count - 1].Click_Out(elements) + '\n';
+                elements++;
+                //--------------------------------------------------- 
+                rt.SelectionStart = rt.Text.Length;
+                rt.ScrollToCaret();
+            }
         }
 
         void del_l_Click(object sender, EventArgs e)
@@ -313,7 +336,13 @@ namespace click
             }
             catch (FormatException)
             { }
-
+            if (del > -1 && click.Count > del)
+            {
+                click.RemoveAt(del);
+                rt.Text = "";
+                for (int i = 0; i < click.Count; i++)
+                    rt.Text = rt.Text + click[i].Click_Out(i) + '\n';
+            }
         }
     }
 }
