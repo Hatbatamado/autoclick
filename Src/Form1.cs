0743de31c4e33572f72ebeb082c12114d8a01ebd
Nakia <nakiaaresz@gmail.com>
2014-07-07 18:42:00 +0200

    layout changes and ADD table in progress

--- Src/Form1.cs
+++ Src/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
@@ -31,7 +32,8 @@ namespace click
             Alap();
         }
 
-        TextBox x, y, repeat;
+        TextBox x, y, repeat, delay;
+        RichTextBox rt;
         private void Alap()
         {
             this.KeyPreview = true; //MUST HAVE for the buttons
@@ -39,8 +41,8 @@ namespace click
             #region Layout
             this.Text = "Auto-clicker by Nakia";
             //---------------------------------------------------
-            this.Location = new Point(Screen.GetWorkingArea(this).Width -700, 150);
-            this.Size = new Size(600, 500);
+            this.Location = new Point(Screen.GetWorkingArea(this).Width - 550, 150);
+            this.Size = new Size(450, 500);
             //---------------------------------------------------
             Button assign = new Button();
             Controls.Add(assign);
@@ -52,14 +54,14 @@ namespace click
             Button add = new Button();
             Controls.Add(add);
             add.Text = "Add";
-            add.Location = new Point(140, 40);
+            add.Location = new Point(140, 100);
             add.Click += add_Click;
             add.TabIndex = 0;
             //---------------------------------------------------
             Button start = new Button();
             Controls.Add(start);
             start.Text = "Start (F7)";
-            start.Location = new Point(300, 10);
+            start.Location = new Point(275, 100);
             start.TabIndex = 2;
             start.Click += start_Click;
             //---------------------------------------------------
@@ -81,17 +83,34 @@ namespace click
             yy.Text = "Y:";
             yy.Location = new Point(10, 45);
             //---------------------------------------------------
+            Label del = new Label();
+            Controls.Add(del);
+            del.Text = "delay:";
+            del.Location = new Point(10, 75);
+            del.Size = new Size(36, 22);
+            //---------------------------------------------------
+            delay = new TextBox();
+            Controls.Add(delay);
+            delay.Location = new Point(55, 72);
+            delay.Size = new Size(75, 22);
+            delay.Text = "1000";
+            //---------------------------------------------------
             Label rep = new Label();
             Controls.Add(rep);
             rep.Text = "Repeat:";
-            rep.Location = new Point(450, 45);
+            rep.Location = new Point(10, 105);
             rep.Size = new Size(45, 22);
             //---------------------------------------------------
             repeat = new TextBox();
             Controls.Add(repeat);
-            repeat.Location = new Point(500, 42);
-            repeat.Size = new Size(50, 22);
+            repeat.Location = new Point(55, 102);
+            repeat.Size = new Size(75, 22);
             repeat.Text = "-1";
+            //---------------------------------------------------
+            rt = new RichTextBox();
+            Controls.Add(rt);
+            rt.Location = new Point(50, 160);
+            rt.Size = new Size(325, 200);
             #endregion
             //---------------------------------------------------
             assign_glob = new GlobalKey(Keys.F6, this);
@@ -151,13 +170,10 @@ namespace click
         Timer time = new Timer();
         bool run = false;
         int db;
-        uint X, Y;
         private void Start()
         {
             db = Convert.ToInt32(repeat.Text);
             Cursor.Position = new Point(Convert.ToInt32(x.Text), Convert.ToInt32(y.Text));
-            uint X = (uint)Cursor.Position.X;
-            uint Y = (uint)Cursor.Position.Y;
             //---------------------------------------------------
             if (!run)
                 run = true;
@@ -177,15 +193,20 @@ namespace click
             }
             else
             {
-                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+                //mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
                 if (db > 0)
                     db--;
             }
         }
 
+        List<Click> click = new List<Click>();
         private void Add()
         {
             //TODO: ADD table & delay
+            uint X = (uint)Cursor.Position.X;
+            uint Y = (uint)Cursor.Position.Y;
+            click.Add(new Click(X, Y, 0, 0));
+            rt.Text = rt.Text + click[click.Count - 1].Click_Out() + '\n';
         }
     }
 }
