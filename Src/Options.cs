1f603de0deff722b282022c7df21bbb15723464c
Nakia <nakiaaresz@gmail.com>
2014-09-03 15:36:55 +0200

    message box position changed

--- Src/Options.cs
+++ Src/Options.cs
@@ -1,17 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace click
 {
     public partial class Options : Form
     {
+        //Messagebox location needs:
+        //http://www.codeproject.com/Tips/472294/Position-a-Windows-Forms-MessageBox-in-Csharp
+        [DllImport("user32.dll")]
+        static extern IntPtr FindWindow(IntPtr classname, string title);
+        [DllImport("user32.dll")]
+        static extern void MoveWindow(IntPtr hwnd, int X, int Y,
+            int nWidth, int nHeight, bool rePaint);
+        [DllImport("user32.dll")]
+        static extern bool GetWindowRect
+            (IntPtr hwnd, out Rectangle rect);
+        //--------------
+
         //TODO: simplify this code and add missing buttons
         public Options()
         {
@@ -23,6 +31,7 @@ namespace click
         private void Design()
         {
             //TODO: simplify/change this code for the default button
+            //maybe move these to Design.cs
             this.Location = new Point(Screen.GetWorkingArea(this).Width - 550, 150);
             this.KeyPreview = true;
             this.KeyDown += Options_KeyDown;
@@ -30,7 +39,7 @@ namespace click
             Label options = new Label();
             options.Font = new Font("Arial", 12, FontStyle.Bold);
             options.Text = "Options";
-            options.Location = new Point(100, 10);
+            options.Location = new Point(110, 10);
             this.Controls.Add(options);
             //--------------
             //Labels
@@ -69,10 +78,24 @@ namespace click
             //---
             Button defaul = new Button();
             this.Controls.Add(defaul);
-            defaul.Location = new Point(60, 225);
+            defaul.Location = new Point(40, 225);
             defaul.Text = "Default";
             defaul.Size = new Size(60, 22);
             defaul.Click += defaul_Click;
+            //---
+            Button save = new Button();
+            this.Controls.Add(save);
+            save.Location = new Point(110, 225);
+            save.Text = "Save";
+            save.Size = new Size(60, 22);
+            save.Click += save_Click;
+            //---
+            Button cancel = new Button();
+            this.Controls.Add(cancel);
+            cancel.Location = new Point(180, 225);
+            cancel.Text = "Cancel";
+            cancel.Size = new Size(60, 22);
+            cancel.Click += cancel_Click;
             //--------------
             //Textboxes
             TextBox d = Designn(new Point(100, 122), new Size(114, 22),
@@ -83,6 +106,44 @@ namespace click
                 Config.Config_settings("process").ToString());
             TextBox s = Designn(new Point(100, 197), new Size(114, 22),
                 Config.Config_settings("speed").ToString());
+        }
+
+        //Messagebox moving method:
+        void FindAndMoveMsgBox(int x, int y, bool repaint, string title)
+        {
+            Thread thr = new Thread(() => // create a new thread
+            {
+                IntPtr msgBox = IntPtr.Zero;
+                // while there's no MessageBox, FindWindow returns IntPtr.Zero
+                while ((msgBox = FindWindow(IntPtr.Zero, title)) == IntPtr.Zero) ;
+                // after the while loop, msgBox is the handle of your MessageBox
+                Rectangle r = new Rectangle();
+                GetWindowRect(msgBox, out r); // Gets the rectangle of the message box
+                MoveWindow(msgBox /* handle of the message box */, x, y,
+                   r.Width - r.X /* width of originally message box */,
+                   r.Height - r.Y /* height of originally message box */,
+                   repaint /* if true, the message box repaints */);
+            });
+            thr.Start(); // starts the thread
+        }
+
+        void cancel_Click(object sender, EventArgs e)
+        {
+            FindAndMoveMsgBox(Screen.GetWorkingArea(this).Width - 575, 225, true, "Cancel");
+            DialogResult result =
+                MessageBox.Show("Are you sure you want to close this without saving?",
+                "Cancel", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+                this.Close();
+            else
+            {
+                //SAVE
+            }
+        }
+
+        void save_Click(object sender, EventArgs e)
+        {
+            
         }
 
         void start_Click(object sender, EventArgs e)
